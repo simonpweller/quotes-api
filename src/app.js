83977f355f7d4ac97f3866ab606a52b3decdc13c
Simon Weller <s.meierbeck@gmail.com>
2020-05-04 22:52:52 +0200

    Make port configurable through environment variables
    This is necessary so the api can be deployed to heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const quotes = require("./quotes");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   const quote = quotes.random();
