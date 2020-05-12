23c20120e654e63c6b3f629d8912960e7efeb26d
Serhazor <sergej.dikun@gmail.com>
2020-05-12 10:37:36 +0100

    added the line for app.js to use router

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use("/", router);
+
 app.listen(port, () => console.log(`Quotes API listening on port ${port}`));
+
+
+
