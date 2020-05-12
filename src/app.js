142ee66dca4ed6336960466779b2617e1faf3e56
Simon Weller <s.meierbeck@gmail.com>
2020-05-12 20:35:52 +0200

    Enable cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./router");
 
-const app = express();
 const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
 
 app.use("/", router);
 
