7908cd7316802a2e5393907296af33f1a354a37f
R Vrishab Jain <jainvrishab@gmail.com>
2020-05-15 22:52:51 +0530

    Fetch quote by ID Issue#10

--- src/router.js
+++ src/router.js
@@ -13,4 +13,17 @@ router.get("/random", (req, res) => {
   res.json(quote);
 });
 
+router.get("/:quoteID", (req, res) => {
+  const quoteID = parseInt(req.params.quoteID);
+  const quotesLength = quotes.length;
+
+  if (0 <= quoteID && quoteID < quotesLength) {
+    res.json(quotes[quoteID]);
+  } else {
+    res.json({
+      err: `Invalid quoteID; quoteID range: [0, ${quotesLength - 1}]`,
+    });
+  }
+});
+
 module.exports = router;
