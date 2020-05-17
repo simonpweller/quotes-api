b07a004a24212ed5b03fdcf0ff7ce1ad63d455fc
Simon Weller <s.meierbeck@gmail.com>
2020-05-17 22:35:25 +0200

    Add test for /random

--- src/router.test.js
+++ src/router.test.js
@@ -4,8 +4,21 @@ const quotes = require("./quotes");
 
 describe("routes", () => {
   describe("/", () => {
-    it("responds with a list of quotes", (done) => {
+    it("should respond with a list of quotes", (done) => {
       request(app).get("/").expect(200, quotes, done);
+    });
+  });
+
+  describe("/random", () => {
+    it("should respond with a quote", async (done) => {
+      const res = await request(app).get("/random");
+      const quote = res.body;
+
+      expect(res.status).toBe(200);
+      expect(quote).toHaveProperty("quoteText");
+      expect(quote).toHaveProperty("quoteAuthor");
+      expect(quote).toHaveProperty("quoteLink");
+      done();
     });
   });
 });
