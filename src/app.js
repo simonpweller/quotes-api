490f72f1eaa0ba7e2ec22fbf0dfcd889559ebb13
Simon Weller <s.meierbeck@gmail.com>
2020-05-17 22:25:53 +0200

    start server in separate index.js file
    supertest will need the app, but won't need it to listen on a port, so the first step is separating these two things.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./router");
 
-const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(cors());
 
 app.use("/", router);
 
-app.listen(port, () => console.log(`Quotes API listening on port ${port}`));
+module.exports = app;
