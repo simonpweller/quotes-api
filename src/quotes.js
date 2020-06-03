fb2b902e99576b073d84ca381a8e7186b788ccc5
Simon Weller <s.meierbeck@gmail.com>
2020-06-03 23:54:14 +0200

    Add programming quotes

--- src/quotes.js
+++ src/quotes.js
@@ -17,7 +17,22 @@ const quotes = [
     quoteText:
       "That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
     quoteAuthor: "Larry Niven",
-    quoteLink: "https://www.goodreads.com/quotes/tag/programming",
+    quoteLink:
+      "https://www.goodreads.com/quotes/204427-that-s-the-thing-about-people-who-think-they-hate-computers",
+  },
+  {
+    quoteText:
+      "A language that doesn't affect the way you think about programming is not worth knowing.",
+    quoteAuthor: "Alan J. Perlis",
+    quoteLink:
+      "https://www.goodreads.com/quotes/393595-a-language-that-doesn-t-affect-the-way-you-think-about",
+  },
+  {
+    quoteText:
+      "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
+    quoteAuthor: "Brian Kernighan",
+    quoteLink:
+      "https://www.goodreads.com/quotes/273375-everyone-knows-that-debugging-is-twice-as-hard-as-writing",
   },
 ];
 
