4f88f47ca2d7c082fbfa9a28452b18b581241a09
Simon Weller <s.meierbeck@gmail.com>
2020-06-03 23:57:52 +0200

    simplify quotes objects and add category property

--- src/quotes.js
+++ src/quotes.js
@@ -1,38 +1,43 @@
 const quotes = [
   {
-    quoteText:
+    text:
       "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.",
-    quoteAuthor: "Muhammad Waseem",
-    quoteLink:
+    author: "Muhammad Waseem",
+    link:
       "https://www.goodreads.com/quotes/841129-give-a-man-a-program-frustrate-him-for-a-day",
+    category: "programming",
   },
   {
-    quoteText:
+    text:
       "I'm not a great programmer; I'm just a good programmer with great habits.",
-    quoteAuthor: "Kent Beck",
-    quoteLink:
+    author: "Kent Beck",
+    link:
       "https://www.goodreads.com/quotes/532211-i-m-not-a-great-programmer-i-m-just-a-good-programmer",
+    category: "programming",
   },
   {
-    quoteText:
+    text:
       "That's the thing about people who think they hate computers. What they really hate is lousy programmers.",
-    quoteAuthor: "Larry Niven",
-    quoteLink:
+    author: "Larry Niven",
+    link:
       "https://www.goodreads.com/quotes/204427-that-s-the-thing-about-people-who-think-they-hate-computers",
+    category: "programming",
   },
   {
-    quoteText:
+    text:
       "A language that doesn't affect the way you think about programming is not worth knowing.",
-    quoteAuthor: "Alan J. Perlis",
-    quoteLink:
+    author: "Alan J. Perlis",
+    link:
       "https://www.goodreads.com/quotes/393595-a-language-that-doesn-t-affect-the-way-you-think-about",
+    category: "programming",
   },
   {
-    quoteText:
+    text:
       "Everyone knows that debugging is twice as hard as writing a program in the first place. So if you're as clever as you can be when you write it, how will you ever debug it?",
-    quoteAuthor: "Brian Kernighan",
-    quoteLink:
+    author: "Brian Kernighan",
+    link:
       "https://www.goodreads.com/quotes/273375-everyone-knows-that-debugging-is-twice-as-hard-as-writing",
+    category: "programming",
   },
 ];
 
